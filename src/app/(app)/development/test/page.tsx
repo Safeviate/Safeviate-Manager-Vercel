cb94640d684918233663e3f582b02b17bd15ff54
Barry Bolton <barry@safeviate.com>
2025-12-09 12:07:29 +0000

    i need a second calendar in that format

--- src/app/(app)/development/test/page.tsx
+++ src/app/(app)/development/test/page.tsx
@@ -6,12 +6,23 @@ import { cn } from '@/lib/utils'
 
 export default function TestPage() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const [date2, setDate2] = React.useState<Date | undefined>(new Date())
+
   return (
-    <div className="grid">
+    <div className="grid grid-cols-2 gap-4">
       <Calendar
         mode="single"
         selected={date}
         onSelect={setDate}
+        className="rounded-md border w-[350px] h-[370px]"
+        classNames={{
+            day_selected: "bg-orange-500 text-white hover:bg-orange-600 focus:bg-orange-600 rounded-md",
+        }}
+      />
+      <Calendar
+        mode="single"
+        selected={date2}
+        onSelect={setDate2}
         className="rounded-md border w-[350px] h-[370px]"
         classNames={{
             day_selected: "bg-orange-500 text-white hover:bg-orange-600 focus:bg-orange-600 rounded-md",
