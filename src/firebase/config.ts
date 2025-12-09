7b230a74259e27b38bfb29ca3e5ce5ddc9b13c6f
Barry Bolton <barry@safeviate.com>
2025-12-09 09:10:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "safeviate-aviation-management",
+  "appId": "1:590769121002:web:59bd3368721013d01fd993",
+  "apiKey": "AIzaSyBNSnhXL7n8C7lxHoVp76M3JP_5kcEvKa8",
+  "authDomain": "safeviate-aviation-management.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "590769121002"
+};
