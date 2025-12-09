2a989ce80c9e9863385d30ba619f7fc0ef23b6ed
Barry Bolton <barry@safeviate.com>
2025-12-09 12:06:00 +0000

    add a calender date picker in this format

--- src/app/(app)/development/test/page.tsx
+++ src/app/(app)/development/test/page.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react'
 import { Calendar } from '@/components/ui/calendar'
+import { cn } from '@/lib/utils'
 
 export default function TestPage() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
@@ -12,6 +13,9 @@ export default function TestPage() {
         selected={date}
         onSelect={setDate}
         className="rounded-md border w-[350px] h-[370px]"
+        classNames={{
+            day_selected: "bg-orange-500 text-white hover:bg-orange-600 focus:bg-orange-600 rounded-md",
+        }}
       />
     </div>
   )
