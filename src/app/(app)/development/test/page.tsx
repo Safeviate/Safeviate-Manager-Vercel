5de5704b53b37fe9a3e8955662d8b142eaad51aa
Barry Bolton <barry@safeviate.com>
2025-12-09 12:01:32 +0000

    Great. Ok do not let the calendar auto size at all

--- src/app/(app)/development/test/page.tsx
+++ src/app/(app)/development/test/page.tsx
@@ -6,12 +6,12 @@ import { Calendar } from '@/components/ui/calendar'
 export default function TestPage() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    <div className="grid w-[350px] h-[370px]">
+    <div className="grid">
       <Calendar
         mode="single"
         selected={date}
         onSelect={setDate}
-        className="rounded-md border"
+        className="rounded-md border w-[350px] h-[370px]"
       />
     </div>
   )
