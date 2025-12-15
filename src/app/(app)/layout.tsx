58b90e3154a776b51a7b8ba0c33bf195477fb5df
Barry Bolton <barry@safeviate.com>
2025-12-15 16:38:17 +0000

    implement

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppHeader } from '@/components/app-header';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
@@ -16,7 +17,9 @@ export default function AppLayout({
         <div className="flex flex-col flex-1 h-screen">
           <AppHeader />
           <SidebarInset className="p-4 lg:p-6 flex-1 overflow-y-auto pb-20 md:pb-4">
-            {children}
+            <div className="w-full max-w-7xl mx-auto">
+              {children}
+            </div>
           </SidebarInset>
           <AppBottomNav />
         </div>
