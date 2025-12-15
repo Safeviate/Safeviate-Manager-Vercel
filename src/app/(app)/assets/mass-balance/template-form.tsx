5c15a782c1f7398eea2a6ce40688d8c5210386ef
Barry Bolton <barry@safeviate.com>
2025-12-15 09:44:14 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/app/(app)/assets/mass-balance/template-form.tsx
+++ src/app/(app)/assets/mass-balance/template-form.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm, useFieldArray, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
