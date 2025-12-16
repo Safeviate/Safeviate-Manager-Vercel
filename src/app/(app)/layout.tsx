39fb920a241e9312b475a381c7aa5bf10182285c
Barry Bolton <barry@safeviate.com>
2025-12-16 18:33:03 +0000

    Can we imlement the zoom level of the configurator to all pages regardle

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppHeader } from '@/components/app-header';
-import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import { SidebarInset } from '@/components/ui/sidebar';
 import { AuthGuard } from '@/components/auth-guard';
 import { AppBottomNav } from '@/components/app-bottom-nav';
 
@@ -16,8 +16,8 @@ export default function AppLayout({
         <AppSidebar />
         <div className="flex flex-col flex-1 h-screen">
           <AppHeader />
-          <SidebarInset className="p-4 lg:p-6 flex-1 overflow-y-auto pb-20 md:pb-4">
-            <div className="w-full max-w-7xl mx-auto h-full">
+          <SidebarInset className="p-4 lg:p-6 flex-1 overflow-auto pb-20 md:pb-4">
+            <div className="w-full max-w-7xl mx-auto h-full min-w-[900px]">
               {children}
             </div>
           </SidebarInset>
