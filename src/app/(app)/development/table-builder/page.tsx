7779fbe17205f287dcea1c5d8e3fa94a16e74549
Barry Bolton <barry@safeviate.com>
2025-12-25 14:18:56 +0000

    ok so we need to reduce that even futher

--- src/app/(app)/development/table-builder/page.tsx
+++ src/app/(app)/development/table-builder/page.tsx
@@ -44,7 +44,7 @@ type TableTemplate = {
 };
 
 const DEFAULT_COL_WIDTH = 120;
-const DEFAULT_ROW_HEIGHT = 20;
+const DEFAULT_ROW_HEIGHT = 10;
 const DEFAULT_FONT_SIZE = 14;
 
 const TablePreview = ({ tableData }: { tableData: TableData }) => {
@@ -620,9 +620,9 @@ const TableBuilderPage = () => {
                                 className="p-0 border border-border bg-muted/50 relative"
                             >
                                 <div className="h-7 flex items-center justify-center p-0">
-                                    <Button variant="ghost" size="icon" className='h-7 w-7' onClick={() => deleteColumn(colIndex)}><Trash2 className="h-4 w-4" /></Button>
+                                    <Button variant="ghost" size="icon" className='h-7 w-7 p-0' onClick={() => deleteColumn(colIndex)}><Trash2 className="h-4 w-4" /></Button>
                                     <SizeInput value={tableData.colWidths[colIndex]} onSave={(newWidth) => updateColWidth(colIndex, newWidth)} />
-                                    <Button variant="ghost" size="icon" className='h-7 w-7' onClick={() => addColumn(colIndex + 1)}><PlusCircle className="h-4 w-4" /></Button>
+                                    <Button variant="ghost" size="icon" className='h-7 w-7 p-0' onClick={() => addColumn(colIndex + 1)}><PlusCircle className="h-4 w-4" /></Button>
                                 </div>
                             </th>
                         ))}
@@ -635,9 +635,9 @@ const TableBuilderPage = () => {
                         {isEditMode && (
                             <th className="p-0 border border-border bg-muted/50 sticky left-0 z-10">
                                <div className="w-32 h-7 flex flex-row items-center justify-center">
-                                    <Button variant="ghost" size="icon" className='h-7 w-7' onClick={() => deleteRow(rowIndex)}><Trash2 className="h-4 w-4" /></Button>
+                                    <Button variant="ghost" size="icon" className='h-7 w-7 p-0' onClick={() => deleteRow(rowIndex)}><Trash2 className="h-4 w-4" /></Button>
                                     <SizeInput value={tableData.rowHeights[rowIndex]} onSave={(newHeight) => updateRowHeight(rowIndex, newHeight)} />
-                                    <Button variant="ghost" size="icon" className='h-7 w-7' onClick={() => addRow(rowIndex + 1)}><PlusCircle className="h-4 w-4" /></Button>
+                                    <Button variant="ghost" size="icon" className='h-7 w-7 p-0' onClick={() => addRow(rowIndex + 1)}><PlusCircle className="h-4 w-4" /></Button>
                                 </div>
                             </th>
                         )}
@@ -721,4 +721,5 @@ const TableBuilderPage = () => {
 };
 
 export default TableBuilderPage;
+
 
