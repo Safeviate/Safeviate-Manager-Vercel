9173c60010211fd1799a06542a5ee38f5cb4fa79
Deane <barry@safeviate.com>
2026-04-04 20:32:58 +0200

    fix: redirect preview api traffic to production

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,6 +20,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
-
